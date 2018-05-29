eb181925695d27a8eaa2f503dad5880ac1f166e5
Peter Lamar <peterrlamar@gmail.com>
2018-05-29 15:39:18 -0400

    Update up.sh

--- up.sh
+++ up.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+docker network create influxdb
 docker run -d --name=influxdb --net=influxdb -p 8086:8086 influxdb
 docker run -p 8888:8888 --net=influxdb chronograf --influxdb-url=http://influxdb:8086
