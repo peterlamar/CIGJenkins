8413f6aa1ba53d4137cb34bdf3cb1a7c9918439e
peterlamar <peterrlamar@gmail.com>
2018-05-29 21:08:09 -0400

    Add jenkins

--- up.sh
+++ up.sh
@@ -3,3 +3,4 @@
 docker network create influxdb
 docker run -d --name=influxdb --net=influxdb -p 8086:8086 influxdb
 docker run -p 8888:8888 --net=influxdb chronograf --influxdb-url=http://influxdb:8086
+docker run -p 8080:8080 -p 50000:50000 -v /home/plamar/dev/jenkins:/var/jenkins_home jenkins
