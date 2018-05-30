f44c19c266c212367a5f54f56b59427bd8f3e2de
peterlamar <peterrlamar@gmail.com>
2018-05-30 05:54:24 -0400

    Ease of use

--- up.sh
+++ up.sh
@@ -2,5 +2,5 @@
 
 docker network create influxdb
 docker run -d --name=influxdb --net=influxdb -p 8086:8086 influxdb
-docker run -p 8888:8888 --net=influxdb chronograf --influxdb-url=http://influxdb:8086
-docker run -p 8080:8080 -p 50000:50000 -v /home/plamar/dev/jenkins:/var/jenkins_home jenkins
+docker run -d -p 8888:8888 --net=influxdb chronograf --influxdb-url=http://influxdb:8086
+docker run -d -p 8080:8080 -p 50000:50000 -v /home/plamar/dev/jenkins:/var/jenkins_home jenkins
