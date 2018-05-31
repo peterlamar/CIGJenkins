22124c66aa1a4bb2c48cc4b9451deff0dcfbaa16
peterlamar <peterrlamar@gmail.com>
2018-05-31 09:20:18 -0400

    Minor fix

--- up.sh
+++ up.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# docker kill $(docker ps -q)
-# docker rm $(docker ps -a -q)
-# docker rmi $(docker images -q)
+# Stop Containers: docker kill $(docker ps -q)
+# Delete Containers: docker rm $(docker ps -a -q)
+# Delete Images: docker rmi $(docker images -q)
 
 docker network create influxdb
 docker run -d --name=influxdb --net=influxdb -p 8086:8086 influxdb
 docker run -d -p 8888:8888 --net=influxdb chronograf --influxdb-url=http://influxdb:8086
-docker run -d -p 8080:8080 -p 50000:50000 -v /home/plamar/dev/jenkins:/var/jenkins_home jenkins/jenkins:latest
+docker run -d -p 8080:8080 --net=influxdb -p 50000:50000 -v /home/plamar/dev/jenkins:/var/jenkins_home jenkins/jenkins:latest
