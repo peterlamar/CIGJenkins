49b4e08d22e04442219a0be54ecf4af24b87ef92
Peter Lamar <peter.r.lamar@gmail.com>
2019-02-26 08:57:55 -0500

    Update influxjenkinschronoup.sh

--- influxjenkinschronoup.sh
+++ influxjenkinschronoup.sh
@@ -7,4 +7,4 @@
 docker network create influxdb
 docker run -d --name=influxdb --net=influxdb -p 8086:8086 influxdb
 docker run -d -p 8888:8888 --net=influxdb chronograf --influxdb-url=http://influxdb:8086
-docker run -d -p 8080:8080 --net=influxdb -p 50000:50000 -v /home/plamar/dev/jenkins:/var/jenkins_home jenkins/jenkins:latest
+docker run -d -p 8080:8080 --net=influxdb -p 50000:50000 jenkins/jenkins:latest
